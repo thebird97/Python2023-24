cf7400da03fe3d9d044ac3f66c346241d53b4927
Balint Madar <birdprogramming@gmail.com>
2023-09-22 19:17:07 +0200

    endoflab2

--- Lab2(Sep22)/lab2.py
+++ Lab2(Sep22)/lab2.py
@@ -1,1 +1,22 @@
 print("hi")
+print('"I\'m"')
+print('"\"learning\""')
+print('""learning""')
+print(0o123)
+print(0x123)
+print(type("2"))
+print(type(2))
+print(round(0.000000000000000000001,22))
+print(round(0.000000000000000000001,23))
+print(0x1A3) #419
+print(2 ** 3)
+print(2 + 3 * 5  ) #okay
+
+a=3.0
+b = 4.0
+print(a ** 2 + b ** 2) ** 0.5
+
+
+kilometers = 12.25 miles = 7.38
+miles_to_kilometers = ### kilometers_to_miles = ###
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers") print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles"
