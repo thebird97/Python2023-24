ad9f56f994873b1c8983b0e0770a158e405b7b11
Balint Madar <birdprogramming@gmail.com>
2023-09-29 18:17:10 +0200

    3rd

---
+++ 03/lab3.py
@@ -0,0 +1,13 @@
+
+
+print("Number: % d %%% 12 , ) , thats it")
+
+#print("episode: {}/{}, time: {}, rep: {}, Session:{:.2}".format("friends", 13, 12.30, 2, 5))
+
+name = "ABX"
+type_of_company = "XYZ"
+print(f"{name} is an {type_of_company}company.")
+
+
+#tuple
+my_tuple = (1,2,3)
