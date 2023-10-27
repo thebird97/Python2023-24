e03f00ee670b6de8859b44e9dffe0efe2648b875
Balint Madar <birdprogramming@gmail.com>
2023-10-27 20:44:04 +0200

    07

--- 07/2fel.py
+++ 07/2fel.py
@@ -1,23 +1,45 @@
 class Stack:
     class_level_num = 777
+
     def __init__(self):
-        self.__stack_list = []
+        self._stack_list = []  # Made protected
 
     def push(self, val):
-        self.__stack_list.append(val)
+        self._stack_list.append(val)
 
     def pop(self):
         try:
-            val = self.__stack_list[-1]
-            del self.__stack_list[-1]
+            val = self._stack_list[-1]
+            del self._stack_list[-1]
             return val
         except IndexError:
             print("Empty")
 
     def getList(self):
-        return self.__stack_list
+        return self._stack_list
 
-my_stack = Stack()
+
+class AddingStack(Stack):
+    def __init__(self):
+        super().__init__()
+        self.__sum = 0
+
+    def push(self, val):
+        self.__sum += val
+        super().push(val)  # Use super() for clarity
+
+    def pop(self):
+        val = super().pop()
+        if val is not None:
+            self.__sum -= val
+        return val
+
+    def get_sum(self):
+        return self.__sum
+
+
+my_stack = AddingStack()
+
 print(type(my_stack))
 my_stack.push(2)
 my_stack.push(3)
@@ -34,3 +56,5 @@ print(my_stack.pop())
 
 print(my_stack.class_level_num)
 print(my_stack.pop())
+
+print(my_stack.get_sum())
